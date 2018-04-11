6e080d377f342cf842515808d0a256e3c5ada9d7
mbjelac <marko.bjelac@softwaresauna.com>
2018-04-11 11:25:45 +0200

    replaced if with let

--- src/i_introduction/_7_Nullable_Types/n07NullableTypes.kt
+++ src/i_introduction/_7_Nullable_Types/n07NullableTypes.kt
@@ -13,24 +13,32 @@ fun test() {
 }
 
 fun todoTask7(client: Client?, message: String?, mailer: Mailer): Nothing = TODO(
-    """
+        """
         Task 7.
         Rewrite JavaCode7.sendMessageToClient in Kotlin, using only one 'if' expression.
         Declarations of Client, PersonalInfo and Mailer are given below.
     """,
-    documentation = doc7(),
-    references = { JavaCode7().sendMessageToClient(client, message, mailer) }
+        documentation = doc7(),
+        references = { JavaCode7().sendMessageToClient(client, message, mailer) }
 )
 
 fun sendMessageToClient(
         client: Client?, message: String?, mailer: Mailer
 ) {
-    if(message == null || client?.personalInfo?.email == null ) return
-    mailer.sendMessage(client.personalInfo.email, message)
+
+//    if (message == null || client?.personalInfo?.email == null) return
+
+    /* instead of if, we can use let: */
+
+    message?.let {
+        client?.personalInfo?.email?.let {
+            mailer.sendMessage(client.personalInfo.email, message)
+        }
+    }
 }
 
-class Client (val personalInfo: PersonalInfo?)
-class PersonalInfo (val email: String?)
+class Client(val personalInfo: PersonalInfo?)
+class PersonalInfo(val email: String?)
 
 interface Mailer {
     fun sendMessage(email: String, message: String)
