deaef99e4d233bbb1d47df2031e99136f1e39a0b
mbjelac <marko.bjelac@softwaresauna.com>
2018-04-11 11:21:29 +0200

    solved koans 4-7

--- src/i_introduction/_7_Nullable_Types/n07NullableTypes.kt
+++ src/i_introduction/_7_Nullable_Types/n07NullableTypes.kt
@@ -25,7 +25,8 @@ fun todoTask7(client: Client?, message: String?, mailer: Mailer): Nothing = TODO
 fun sendMessageToClient(
         client: Client?, message: String?, mailer: Mailer
 ) {
-    todoTask7(client, message, mailer)
+    if(message == null || client?.personalInfo?.email == null ) return
+    mailer.sendMessage(client.personalInfo.email, message)
 }
 
 class Client (val personalInfo: PersonalInfo?)
