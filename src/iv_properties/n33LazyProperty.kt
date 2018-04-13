daf5f1719351ea02689617e491225d1f67e99d9d
mbjelac <marko.bjelac@softwaresauna.com>
2018-04-13 15:10:07 +0200

    solved koans 32-35

--- src/iv_properties/n33LazyProperty.kt
+++ src/iv_properties/n33LazyProperty.kt
@@ -3,11 +3,19 @@ package iv_properties
 import util.TODO
 
 class LazyProperty(val initializer: () -> Int) {
-    val lazy: Int = todoTask33()
+    private var lazyNullable:Int? = null
+        get() {
+            if (field == null) {
+                field = initializer()
+            }
+
+            return field
+        }
+    val lazy: Int get() = lazyNullable!!
 }
 
 fun todoTask33(): Nothing = TODO(
-    """
+        """
         Task 33.
         Add a custom getter to make the 'lazy' val really lazy.
         It should be initialized by the invocation of 'initializer()'
@@ -15,5 +23,5 @@ fun todoTask33(): Nothing = TODO(
         You can add as many additional properties as you need.
         Do not use delegated properties yet!
     """,
-    references = { LazyProperty({ 42 }).lazy }
+        references = { LazyProperty({ 42 }).lazy }
 )
