8064574b98f03146c96c3c0e1447fe6c04f6cc48
mbjelac <marko.bjelac@softwaresauna.com>
2018-04-16 11:58:16 +0200

    solved koans 36-40

--- src/v_builders/n37StringAndMapBuilders.kt
+++ src/v_builders/n37StringAndMapBuilders.kt
@@ -1,7 +1,9 @@
 package v_builders
 
+import com.google.common.collect.ImmutableMap
 import util.TODO
 import java.util.*
+import kotlin.collections.HashMap
 
 fun buildStringExample(): String {
     fun buildString(build: StringBuilder.() -> Unit): String {
@@ -29,11 +31,17 @@ fun todoTask37(): Nothing = TODO(
 )
 
 fun task37(): Map<Int, String> {
-    todoTask37()
-//    return buildMap {
-//        put(0, "0")
-//        for (i in 1..10) {
-//            put(i, "$i")
-//        }
-//    }
+
+    fun buildMap(mapBuilder: MutableMap<Int, String>.() -> Unit): Map<Int, String> {
+        val map = HashMap<Int, String>()
+        map.mapBuilder()
+        return ImmutableMap.copyOf(map)
+    }
+
+    return buildMap {
+        put(0, "0")
+        for (i in 1..10) {
+            put(i, "$i")
+        }
+    }
 }
--- src/v_builders/n39HtmlBuilders.kt
+++ src/v_builders/n39HtmlBuilders.kt
@@ -6,23 +6,23 @@ import v_builders.data.getProducts
 import v_builders.htmlLibrary.*
 
 fun getTitleColor() = "#b9c9fe"
-fun getCellColor(row: Int, column: Int) = if ((row + column) %2 == 0) "#dce4ff" else "#eff2ff"
+fun getCellColor(row: Int, column: Int) = if ((row + column) % 2 == 0) "#dce4ff" else "#eff2ff"
 
 fun todoTask39(): Nothing = TODO(
-    """
+        """
         Task 39.
         1) Fill the table with the proper values from products.
         2) Color the table like a chess board (using getTitleColor() and getCellColor() functions above).
         Pass a color as an argument to functions 'tr', 'td'.
         You can call the 'main' function in the 'htmlDemo.kt' to see the rendered table.
     """,
-    documentation = doc39()
+        documentation = doc39()
 )
 
 fun renderProductTable(): String {
     return html {
         table {
-            tr {
+            tr(color = getTitleColor()) {
                 td {
                     text("Product")
                 }
@@ -34,7 +34,20 @@ fun renderProductTable(): String {
                 }
             }
             val products = getProducts()
-            todoTask39()
+
+            products.forEachIndexed { index, product ->
+                tr {
+                    td(color = getCellColor(index, 0)) {
+                        text(product.description)
+                    }
+                    td(color = getCellColor(index, 1)) {
+                        text(product.price)
+                    }
+                    td(color = getCellColor(index, 2)) {
+                        text(product.popularity)
+                    }
+                }
+            }
         }
     }.toString()
 }
